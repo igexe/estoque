b1bee7db29389f446afa1eaf769a373d85e6e370
igexe <igthedev@gmail.com>
2024-03-08 22:00:20 -0300

    criação dos arquivos base para funcionamento do sistema de estoque

---
+++ cadastro.py
@@ -0,0 +1,34 @@
+import pandas as pd
+
+def ad():
+    codigo=1
+    try:
+        estoque_df=pd.read_csv('teste.csv')
+        codigo=estoque_df['codigo'].iloc[-1]+1
+    except FileNotFoundError:
+        db=open('teste.csv','w')
+        db.close()
+        estoque_df=pd.DataFrame({
+            'codigo':[codigo],
+            'marca':[input('\ndigite a marca do produto:\n')],
+            'produto':[input('\ndigite o nome do produto:\n')],
+            'idade':[input('\ndigite\nadt se o produto for adulto\ninf se o produto for infantil\n')],
+            'sexo':[input('\ndigite\nmasc so o produto for masculino\nfem se o produto for feminino\n')],
+            'quantidade':[int(input('\ndigite quantidade de produtos chegaram:\n'))],
+            'valor':[float(input('\ndigite o valor do produto:\n'))],
+            'referencia':[input('\ndigite a referencia do produto:\n')]
+        })
+        estoque_df.to_csv('teste.csv',index=False)
+        return 0
+
+    estoque_df=estoque_df._append({
+            'codigo':codigo,
+            'marca':input('\ndigite a marca do produto:\n'),
+            'produto':input('\ndigite o nome do produto:\n'),
+            'idade':input('\ndigite\nadt se o produto for adulto\ninf se o produto for infantil\n'),
+            'sexo':input('\ndigite\nmasc so o produto for masculino\nfem se o produto for feminino\n'),
+            'quantidade':int(input('\ndigite quantidade de produtos chegaram:\n')),
+            'valor':float(input('\ndigite o valor do produto:\n')),
+            'referencia':input('\ndigite a referencia do produto:\n')
+        },ignore_index=True)
+    estoque_df.to_csv('teste.csv',index=False)
---
+++ main.py
@@ -0,0 +1,3 @@
+from cadastro import ad
+
+ad()
