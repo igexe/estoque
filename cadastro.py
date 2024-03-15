9a55dcc621bf1b0d6d1da2a82667b526400949f6
igexe <igthedev@gmail.com>
2024-03-15 19:49:02 -0300

    codigo para criação do arquivo csv que guardara produtos cadastrados e cadastrar novos produtos em estoque

--- cadastro.py
+++ cadastro.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-def ad():
+def ad(ref=0):
     codigo=1
     try:
         estoque_df=pd.read_csv('teste.csv')
@@ -21,6 +21,20 @@ def ad():
         estoque_df.to_csv('teste.csv',index=False)
         return 0
 
+    if ref==0:
+        estoque_df=estoque_df._append({
+                'codigo':codigo,
+                'marca':input('\ndigite a marca do produto:\n'),
+                'produto':input('\ndigite o nome do produto:\n'),
+                'idade':input('\ndigite\nadt se o produto for adulto\ninf se o produto for infantil\n'),
+                'sexo':input('\ndigite\nmasc so o produto for masculino\nfem se o produto for feminino\n'),
+                'quantidade':int(input('\ndigite quantidade de produtos chegaram:\n')),
+                'valor':float(input('\ndigite o valor do produto:\n')),
+                'referencia':input('\ndigite a referencia do produto:\n')
+            },ignore_index=True)
+        estoque_df.to_csv('teste.csv',index=False)
+        return 0
+
     estoque_df=estoque_df._append({
             'codigo':codigo,
             'marca':input('\ndigite a marca do produto:\n'),
@@ -29,7 +43,8 @@ def ad():
             'sexo':input('\ndigite\nmasc so o produto for masculino\nfem se o produto for feminino\n'),
             'quantidade':int(input('\ndigite quantidade de produtos chegaram:\n')),
             'valor':float(input('\ndigite o valor do produto:\n')),
-            'referencia':input('\ndigite a referencia do produto:\n')
+            'referencia':ref
         },ignore_index=True)
+
     estoque_df.to_csv('teste.csv',index=False)
     
