1185262e0782326cf3072a92550f43fab86661ed
igexe <igthedev@gmail.com>
2024-04-01 19:22:18 -0300

    modificação para utilização dos metodos de busca

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 from cadastro import ad
+from leitura import show
 
-ad()
+while(True):
+    ok:int=int(input('\n1 para cadastrar produto\n2 para pesquisar produtos cadastrados\n'))
+    if(ok==1):
+        ad()
+    elif(ok==2):
+        show()
+    else:
+        break
