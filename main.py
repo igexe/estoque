d015a884bb0df273af1e488eb1f590a5b66259a7
igexe <igthedev@gmail.com>
2024-04-12 21:33:14 -0300

    modificação do main para comportar parte de modificação

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from cadastro import ad
 from leitura import show
+from modifica import update
 
 while(True):
-    ok:int=int(input('\n1 para cadastrar produto\n2 para pesquisar produtos cadastrados\n'))
-    if(ok==1):
-        ad()
-    elif(ok==2):
-        show()
-    else:
-        break
+    ok:int=int(input('\n1 para cadastrar produto\n2 para pesquisar produtos cadastrados\n3 para modificar um produto\n'))
+    match(ok):
+        case 1:
+            ad()
+        case 2:
+            show()
+        case 3:
+            update()
+        case _:
+            print('\nação invalida\n')
